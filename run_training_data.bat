08ab3bfb0cd0833e8a41338889b33199a24a0d0e
oskari6 <oskarisulkakoski@gmail.com>
2024-08-15 07:17:45 +0300

    folder structure update

--- run_training_data.bat
+++ run_training_data.bat
@@ -4,7 +4,7 @@ set logFile=C:\Temp\Excel_script\logs\training_data.log
 cd /d C:\Temp\Excel_script\.venv\Scripts
 call activate
 
-cd /d C:\Temp\Excel_script\polar api\accesslink-example-python
+cd /d C:\Temp\Excel_script\polar api
 python training_data.py >> "%logFile%" 2>&1
 
 echo %date% %time% >> "%logFile%"
