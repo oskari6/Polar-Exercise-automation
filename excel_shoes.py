ec04acd253f18c50a79e8905a7756d68f1e8e70f
oskari6 <oskarisulkakoski@gmail.com>
2024-09-21 08:35:45 +0300

    fix for shoes

--- excel_shoes.py
+++ excel_shoes.py
@@ -5,36 +5,36 @@ def extract_and_move_values():
     ws = wb.sheets.active
 
     source_range = ws.range("J2:J368")
-    letter_range = ws.range("Q2:Q5")
-    target_range = ws.range("R2:R5")
+    letter_range = ws.range("Q2:Q6")
+    target_range = ws.range("R2:R6")
 
     for cell in target_range:
         cell.value = 0
 
     for cell in source_range:
-        source_value = cell.value
+        if cell.value is None:
+            break
+        letter_value = cell.value
         row = cell.row
-        distance_value = ws.range(f"D{row}").value
 
-        if source_value:
-            parts = source_value.split(",")
+        if "," in letter_value:
+            parts = letter_value.split(",")
             for part in parts:
-                part = part.strip() 
-                if '-' in part:
-                    letter, num = part.split("-")
-                    num = int(num) 
-                    for letter_cell in letter_range:
-                        if letter_cell.value.strip() == letter.strip():
-                            target_cell = letter_cell.offset(0, 1)
-                            target_cell.value += num
-                            break
-                else:
-                    for letter_cell in letter_range:
-                        if letter_cell.value.strip() == part.strip():
-                            target_cell = letter_cell.offset(0, 1)
-                            if distance_value:
-                                target_cell.value += distance_value
-                            break
+                part = part.strip()
+                letter_part, num = part.split("-")
+                num = float(num) 
+                for letter in letter_range:
+                    if letter.value.strip() == letter_part.strip():
+                        target_cell = letter.offset(0, 1)
+                        target_cell.value += num
+                        break
+        else:
+            distance_value = ws.range(f"D{row}").value
+            for letter in letter_range:
+                if letter.value.strip() == letter_value.strip():
+                    target_cell = letter.offset(0, 1)
+                    target_cell.value += distance_value
+                    break
 
 if __name__ == "__main__":
     xw.Book("diary_2024.xlsm").set_mock_caller()
